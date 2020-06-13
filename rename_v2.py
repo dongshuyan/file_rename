fca947ee87a46013c4be24771b06b2b26b6c29b3
dongshuyan <sauterne@qq.com>
2020-06-13 12:38:50 +0800

    correct mistask [XX]

--- rename_v2.py
+++ rename_v2.py
@@ -4,18 +4,50 @@ import sys
 import re
 lan=""
 def findnum(name):
+    
     num=re.findall(r" ([0-9][0-9]) ",name)
-    if len(num)==0:
-        num=re.findall(r"\[([0-9][0-9])\]",name)
-    if len(num)==0:
-        num=re.findall(r"-([0-9][0-9])",name)
-    if len(num)==0:
-        num=re.findall(r"第([0-9][0-9])話",name)
-    if len(num)==0:
-        num=re.findall(r"第([0-9][0-9])话",name)
-    if len(num)==0:
-        num=re.findall(r"([0-9][0-9])",name)
-    return num[0]
+    if len(num)!=0:
+        sty=" "+num[0]+" "
+        stz=" XX "
+        stx=" "+num[0]+" "
+        return num[0],sty,stz,stx
+
+    num=re.findall(r"\[([0-9][0-9])\]",name)
+    if len(num)!=0:
+        sty="\["+num[0]+"\]"
+        stz="[XX]"
+        stx="["+num[0]+"]"
+        return num[0],sty,stz,stx
+
+    num=re.findall(r"-([0-9][0-9])",name)
+    if len(num)!=0:
+        sty='-'+num[0]
+        stz="-XX"
+        stx="-"+num[0]
+        return num[0],sty,stz,stx
+
+    num=re.findall(r"第([0-9][0-9])話",name)
+    if len(num)!=0:
+        sty='第'+num[0]+'話'
+        stz="第XX話"
+        stx='第'+num[0]+'話'
+        return num[0],sty,stz,stx
+
+    num=re.findall(r"第([0-9][0-9])话",name)
+    if len(num)!=0:
+        sty='第'+num[0]+'话'
+        stz="第XX话"
+        stx='第'+num[0]+'话'
+        return num[0],sty,stz,stx
+
+    num=re.findall(r"([0-9][0-9])",name)
+    if len(num)!=0:
+        sty=num[0]
+        stz="XX"
+        stx=num[0]
+        return num[0],sty,stz,stx
+        
+    return num[0],sty,stz,stx
 
 def findtcsc(name):
     num=re.findall(r".tc.ass",name)
@@ -26,18 +58,29 @@ def findtcsc(name):
         return ".sc.ass"
     return ".ass"
 
-def findname(num,laypath):
+def findname(num,laypath,sty,stz,stx):
     path = os.listdir(laypath)
     for file_ in path:
         if os.path.isfile(laypath+file_):
             if file_.find('.mkv') >=0 and file_[0]!='.' and file_.find('.mp4') and file_.find('.m4v') and file_.find('.mov') and file_.find('.avi') and file_.find(num):
-                sty="\["+num+"\]"
+                res=re.findall(sty,file_)
+                if not (len(res)==0):
+                    #print("file-name=",file_)
+                    #print("sty=",sty)
+                    #print("stz=",stz)
+
+                    newlay=file_.replace(stx,stz,1)
+                    newlay=newlay.split('.')[0]
+                    print("newlay=",newlay)
+                    return newlay
+                '''
+                sty="["+num+"]"
                 res=re.findall(sty,file_)
                 if not (len(res)==0):
                     newlay=file_.replace(sty,"[XX]",1)
                     newlay=newlay.split('.')[0]
                     return newlay
-                sty='-'+num+']'
+                sty='-'+num
                 res=re.findall(sty,file_)
                 if not (len(res)==0):
                     newlay=file_.replace(sty,"-XX",1)
@@ -61,6 +104,7 @@ def findname(num,laypath):
                     newlay=file_.replace(sty,"第XX話",1)
                     newlay=newlay.split('.')[0]
                     return newlay
+                '''
     return 'NOT_FOUND_XX'
 
 def rename(path,laypath):
@@ -68,12 +112,15 @@ def rename(path,laypath):
     for file_ in files:
         if os.path.isfile(path+file_):
             if file_.find('.ass') >=0 and file_[0]!='.' :
-                num=findnum(file_)
+                num,sty,stz,stx=findnum(file_)
                 if len(re.findall(r"CN",lan)) >0:
                     print("找到第",num,"话字幕:",file_)
                 elif len(re.findall(r"EN",lan))>0:
                 	print("Find the "+num.strip()+"th video subtitle:"+file_)
-                layout=findname(num,laypath)
+                layout=findname(num,laypath,sty,stz,stx)
+                #print("num=",num)
+                #print("laypath=",laypath)
+                #print("layout=",layout)
                 #print("",layout)#print("",layout)
                 newname=layout.replace('XX',str(num),100)+findtcsc(file_)
                 #print("old name= ",file_,"\n new name=",newname,"\n")
